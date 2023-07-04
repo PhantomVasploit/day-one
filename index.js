1ec2db3852716cb203b846bc1bcef531567ef6a3
Paul-Sanga <paulsanganyamawi@gmail.com>
2023-07-04 17:15:02 +0300

    Even number logic complete

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const readLine = require('readline');
-let input;
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-const rl = readLine.createInterface({
-    input: process.stdin,
-    output: process.stdout
+rl.question('Enter number: ', (answer) => {
+    const input = parseInt(answer)
+  if(!isNaN(input)){
+    if(input % 2 == 0){
+        console.log(`${answer} is even!`);
+    }else{
+        console.log(`${answer} is odd!`)
+    }
+  }else{
+    console.log(`${answer} is not a number!`);
+  }
+  rl.close();
 });
